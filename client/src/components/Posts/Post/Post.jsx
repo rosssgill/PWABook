432355918194c47b494206cf076140c968e2dabb
Ross Stephenson-Gill <rossstephensongill@gmail.com>
2022-08-14 04:12:18 +0100

    Fixed bug where delete and edit button wouldn't show for google users posts

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -50,7 +50,7 @@ function Post({ post, setCurrentId }) {
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
-      {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+      {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
         <div className={classes.overlay2}>
           <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
             <MoreHorizIcon fontSize="medium" />
@@ -81,7 +81,7 @@ function Post({ post, setCurrentId }) {
         >
           <Likes />
         </Button>
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
           <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small" />
             Delete
