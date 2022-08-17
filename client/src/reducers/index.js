535985753c80db9690c3df39abdf904cfec9eee8
Ross Stephenson-Gill <rossstephensongill@gmail.com>
2022-08-17 08:32:55 +0100

    Remove client (preparing for git submodule implementation)

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-import posts from './posts';
-import auth from './auth';
-
-export default combineReducers({
-  posts: posts,
-  auth: auth,
-});
